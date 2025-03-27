fe274369d3875cf9be84efbd3a3ab4306491b56b
nazartsokalo <nazartsokalo18@gmail.com>
2025-03-27 14:01:51 +0100

    Second Part

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Item coke = new Item("drink", "coke", 1.50);
-        coke.printItem();
-        coke.setSize("large");
-        coke.printItem();
+//        Item coke = new Item("drink", "coke", 1.50);
+//        coke.printItem();
+//        coke.setSize("large");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping", "avocado", 2 );
+//        avocado.printItem();
 
-        Item avocado = new Item("Topping", "avocado", 2 );
-        avocado.printItem();
+//        Burger burger = new Burger("regular", 4.00);
+//        burger.addToppings("cheese", "bacon", "mayo");
+//        burger.printItem();
+
+//        MealOrder regularMeal = new MealOrder();
+//        regularMeal.addBurgerToppings("Cheese", "Bacon", "Mayo");
+//        regularMeal.setDrinkSize("Large");
+//        regularMeal.printItemizedList();
+
+        MealOrder secondMeal = new MealOrder("turkey", "fanta", "chili");
+        secondMeal.addBurgerToppings("LETTUCE", "Cheese", "Kechup");
+        secondMeal.setDrinkSize("Small");
+        secondMeal.printItemizedList();
     }
 }
